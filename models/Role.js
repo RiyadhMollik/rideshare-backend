d8aa851b798759968b19cd14b526c2c286244c93
tabrid <mollik15-5096@diu.edu.bd>
2025-07-19 08:57:22 +0600

    update

--- models/Role.js
+++ models/Role.js
@@ -2,7 +2,7 @@ const { DataTypes } = require("sequelize");
 const sequelize = require('../config/database');
 const Permission = require("./Permission");
 
-const Role = sequelize.define("Role", {
+const Role = sequelize.define("role", {
   id: {
     type: DataTypes.INTEGER,
     primaryKey: true,
