a67695ccf7358884b2ed797281212640e1cc6d29
tabrid <mollik15-5096@diu.edu.bd>
2025-07-23 21:50:52 +0600

    update

--- graphql/resolvers/rideResolvers.js
+++ graphql/resolvers/rideResolvers.js
@@ -104,7 +104,7 @@ const resolvers = {
               const bids = Array.isArray(ride.bids) ? ride.bids : JSON.parse(ride.bids) ;
               bids.forEach(bid => console.log("bid.riderId:", bid.riderId));
               bids.forEach(bid => console.log("user.user_id:", user.user_id));
-              return bids.some(bid => bid.riderId === 4710);
+              return bids.some(bid => bid.riderId === 6462);
             })
             .map(ride => ({
               ...ride.toJSON(),
